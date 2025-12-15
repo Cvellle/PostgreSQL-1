330f2d0a7599c05d5047b7b7856a00faed2de4ce
cvellle <cuekisa@yahoo.com>
2025-12-15 19:25:19 +0100

    files for vercel

--- api/index.ts
+++ api/index.ts
@@ -1,51 +1,48 @@
-import app from "./app";
+import express, { Request, Response } from "express";
+import mealRoutes from "../routes/meal.routes";
+
+import { sql } from "../config/db";
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/ingredients", async (req: Request, res: Response) => {
+  try {
+    const rows = await sql`
+      SELECT * FROM ingredients
+      ORDER BY id ASC
+    `;
+
+    console.log("DB RESPONSE:", rows);
+
+    return res.json(rows);
+  } catch (error: any) {
+    console.error("NEON ERROR:", {
+      message: error.message,
+      code: error.code,
+      sourceError: error.sourceError,
+      stack: error.stack,
+    });
+
+    return res.status(500).json({
+      error: error.message || "Unknown error",
+    });
+  }
+});
+
+// health
+// app.get("/health", (_req, res) => res.json({ ok: true }));
+// preetify
+app.set("json spaces", 2);
+// routes
+app.use("/meals", mealRoutes);
+// app.use("/meal-items", mealItemRoutes);
+
+export default app;
 
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
 );
 
 module.exports = app;
-
-// import express, { Request, Response } from "express";
-// import { neon } from "@neondatabase/serverless";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const app = express();
-// const sql = neon(process.env.DATABASE_URL!);
-
-// interface Ingredient {
-//   id: number;
-//   name: string;
-//   quantity: number | null;
-//   created_at: string;
-// }
-
-// app.get("/ingredients", async (req: Request, res: Response) => {
-//   try {
-//     const rows = await sql`
-//       SELECT * FROM ingredients
-//       ORDER BY id ASC
-//     `;
-
-//     console.log("DB RESPONSE:", rows); // <-- See exactly what you get
-
-//     return res.json(rows);
-//   } catch (error: any) {
-//     console.error("NEON ERROR:", {
-//       message: error.message,
-//       code: error.code,
-//       sourceError: error.sourceError,
-//       stack: error.stack,
-//     });
-
-//     return res.status(500).json({
-//       error: error.message || "Unknown error",
-//     });
-//   }
-// });
-
-// app.listen(3001, () => console.log("Server running at http://localhost:3001"));
