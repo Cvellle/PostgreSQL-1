24c96d333e81c9f709e5709338922af77908ef51
cvellle <cuekisa@yahoo.com>
2026-01-31 22:59:17 +0100

    get me

--- api/index.ts
+++ api/index.ts
@@ -6,37 +6,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { sql } from "../config/db";
 
-// testing
-import { NextFunction } from "express";
-import jwt from "jsonwebtoken";
-
-interface AuthRequest extends Request {
-  user?: any;
-}
-
-export function authenticateJWT(
-  req: AuthRequest,
-  res: Response,
-  next: NextFunction,
-) {
-  console.log(123, req.cookies.jwt);
-  const token = req.cookies?.jwt;
-
-  if (!token) {
-    return res.status(401).json({ error: "Unauthorized, token missing" });
-  }
-
-  try {
-    const secret = process.env.ACCESS_TOKEN_SECRET || "your-secret";
-    const decoded = jwt.verify(token, secret);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ error: "Unauthorized, invalid token" });
-  }
-}
-//
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,9 +1,45 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller";
+import {
+  registerUser,
+  loginUser,
+  getCurrentUser,
+} from "../controllers/auth.controller";
+import { Request, Response } from "express";
 
 const router = express.Router();
 
+// testing
+import { NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+  user?: any;
+}
+
+export function authenticateJWT(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = req.cookies?.jwt;
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized, token missing" });
+  }
+
+  try {
+    const secret = process.env.ACCESS_TOKEN_SECRET || "your-secret";
+    const decoded = jwt.verify(token, secret);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Unauthorized, invalid token" });
+  }
+}
+//
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", authenticateJWT, getCurrentUser);
 
 export default router;
