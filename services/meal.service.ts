e497b812c249303a6fcfa8e6f7cb4a28f6e56602
cvellle <cuekisa@yahoo.com>
2026-03-09 02:34:55 +0100

    meals repository

--- services/meal.service.ts
+++ services/meal.service.ts
@@ -1,8 +1,10 @@
-import { getMealRawData } from "../repositories/meals.repo";
+import { createMealsRepository } from "../repositories/meals.repo";
 import { calculateMealNutrients } from "../utils/nutritientsCalculator";
 
+const mealsRepo = createMealsRepository();
+
 export async function getMealDetails(mealId: number) {
-  const rows = await getMealRawData(mealId);
+  const rows = await mealsRepo.getRawData(mealId);
   if (!rows || rows.length === 0) return null;
   const result = calculateMealNutrients(rows);
   return result;
